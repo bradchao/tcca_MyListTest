a0981a383d3b75bf1f6cc487ae6956e3a366a87a
Brad <brad@brad.tw>
2017-07-26 19:45:11 +0800

    2017072603

--- app/src/main/java/tw/brad/android/games/mylisttest/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mylisttest/MainActivity.java
@@ -2,12 +2,30 @@ package tw.brad.android.games.mylisttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+        initListView();
     }
+
+
+    private void initListView(){
+        SimpleAdapter adapter = new SimpleAdapter(
+                this, data, R.layout.layout_item, from, to);
+        list.setAdapter(adapter);
+
+
+    }
+
+
 }
